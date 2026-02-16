3e0087f67bc74a6c38000cb86c27d3f864ba6db0
OmerHakanKilic <omerhakankilic04@gmail.com>
2026-02-16 22:21:47 +0000

    Godot Project deleted. Basic window logic implemented

--- Python Code/01_tab_demo.py
+++ Python Code/01_tab_demo.py
@@ -1,19 +1,74 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import (
+    QApplication,
+    QHBoxLayout,
+    QLabel,
+    QMainWindow,
+    QPushButton,
+    QStackedWidget,
+    QVBoxLayout,
+    QWidget,
+)
 
 
-class MyWindow(QMainWindow):
-    def __init__(self) -> None:
+class MapPage(QWidget):
+    def __init__(self):
         super().__init__()
-        self.setWindowTitle(" Demo for 2/17/2026")
-        self.main
+        layout = QVBoxLayout()
+        layout.addWidget(QLabel("<h1>This is the Machine Learning Page</h1>"))
+        self.setLayout(layout)
 
-    pass
+
+class MLPage(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        layout.addWidget(QLabel("<h1>This is the Map Page</h1>"))
+        self.setLayout(layout)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(300, 300, 500, 500)
+        self.stack = QStackedWidget()
+        self.initUI()
+
+    def initUI(self):
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+
+        self.homeButton = QPushButton("Home Tab")
+        self.machineButton = QPushButton("Machine learning Tab")
+
+        self.mapPage = MapPage()
+        self.mlpage = MLPage()
+
+        self.stack.addWidget(self.mlpage)
+        self.stack.addWidget(self.mapPage)
+
+        self.homeButton.clicked.connect(lambda: self.stack.setCurrentIndex(0))
+        self.machineButton.clicked.connect(lambda: self.stack.setCurrentIndex(1))
+
+        masterLayout = QVBoxLayout()
+
+        topBar = QHBoxLayout()
+        topBar.addWidget(self.homeButton)
+        topBar.addWidget(self.machineButton)
+
+        masterLayout.addLayout(topBar)
+        masterLayout.addWidget(self.stack)
+
+        central_widget.setLayout(masterLayout)
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
 
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MyWindow()
-    window.show()
-    sys.exit(app.exec())
+    main()
